241319a5897256ded6e0d7a1242177788c3cff93
cokoyoh <charlesokoyoh@gmail.com>
2022-02-16 11:50:38 +0300

    Clean up

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var collection_1 = require("./collection");
-var collection = new collection_1.Colletion(3);
-collection.next(1);
-collection.next(10);
-collection.next(3);
-var value = collection.next(5);
-console.log({ value: value });
